a89a2a6509a8473c8c0ea47c09a6801e9420bcb0
malacridaanna <malacridaanna@motrust.com>
2023-03-04 16:33:59 +0600

    commit all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState({
+    userName: "",
+    userEmail: "",
+    userMessage: "",
+  });
+  const [data, setData] = useState("");
+  const [error, setError] = useState(null);
+  const { userName, userEmail, userMessage } = user;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios
+      .post("http://localhost:4000/user", user)
+      .then((res) => {
+        setData(res.data.message);
+        setError(null);
+      })
+      .catch((err) => {
+        setData("");
+        setError(err.response.data.message);
+      });
+    setUser({
+      userName: "",
+      userEmail: "",
+      userMessage: "",
+    });
+  };
+
+  const onChangeHandler = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Registration</h2>
+      <h3 style={{ backgroundColor: error ? "res" : data ? "black" : null }}>
+        {error ? error : data}
+      </h3>
+      <div className="Container">
+        <form onSubmit={handleSubmit}>
+          <span>
+            <label htmlFor="userName">Username</label>
+            <input
+              type="text"
+              value={userName}
+              onChange={(e) => onChangeHandler(e)}
+              name="userName"
+              id="userName"
+              placeholder="Enter your username"
+            />
+          </span>
+          <span>
+            <label htmlFor="userEmail">Email</label>
+            <input
+              type="email"
+              value={userEmail}
+              onChange={(e) => onChangeHandler(e)}
+              name="userEmail"
+              id="userEmail"
+              placeholder="Enter your email"
+            />
+          </span>
+          <span>
+            <label htmlFor="userMessage">Message</label>
+            <textarea
+              name="userMessage"
+              value={userMessage}
+              onChange={(e) => onChangeHandler(e)}
+              id="userMessage"
+              cols="30"
+              rows="10"
+            >
+              Enter your message here
+            </textarea>
+          </span>
+          <button type="submit">Registration</button>
+        </form>
+      </div>
     </div>
   );
 }
